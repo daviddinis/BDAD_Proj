d8fa5ebb1c7ef89cbf538c3f9f1b9148a888e6d3
Unknown <up201705049@fe.up.pt>
2019-05-24 00:40:10 +0100

    Triggers acabados

---
+++ testarAtualizarVista.sql
@@ -0,0 +1,3 @@
+INSERT INTO ServicoComFuncionarios(ServiceID, IDfuncionario, IDInterno) VALUES(13, 15, 5);
+INSERT INTO ServicoComFuncionarios(ServiceID, IDfuncionario, IDInterno) VALUES(13, 26, 6);
+INSERT INTO ServicoComFuncionarios(ServiceID, IDfuncionario, IDInterno) VALUES(10, 54, 9);
