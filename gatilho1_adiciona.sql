ee3c173e10295a6d9fb1c7f6f437a7fe8a7ec413
margarida <mar.a.pinho@hotmail.com>
2019-05-26 16:10:23 +0100

    gatilho1 juntado

--- gatilho1_adiciona.sql
+++ gatilho1_adiciona.sql
@@ -1,4 +1,4 @@
--- Rating Medio Trigger
+-- 1.1 Rating Medio Trigger
 
 DROP TRIGGER IF EXISTS RatingMedio;
 
@@ -10,4 +10,45 @@ BEGIN
     UPDATE Servicos
     SET RatingMedio = (SELECT AVG(Rating) FROM Visitas WHERE ServiceID = NEW.ServiceID)
     WHERE ServiceID = NEW.ServiceID;
+END;
+
+-- 1.2 SalarioPagamentos Trigger
+
+DROP TRIGGER IF EXISTS SalarioPagamentos;
+
+CREATE TRIGGER SalarioPagamentos
+AFTER INSERT ON RegistoPagamentos
+BEGIN    
+    UPDATE RegistoPagamentos
+    SET Salario = Bonus +
+    (SELECT SalarioBase FROM Especialidades WHERE NomeEspecialidade = 
+    (SELECT NomeEspecialidade FROM Funcionarios WHERE IDpessoa = NEW.ID)) +
+    HorasExtra *
+    (SELECT ExtraHora FROM Especialidades WHERE NomeEspecialidade = 
+    (SELECT NomeEspecialidade FROM Funcionarios WHERE IDpessoa = NEW.ID))
+    WHERE ID = NEW.ID AND IDmes = NEW.IDmes;
+END;
+
+-- 1.3 Idade Clientes Trigger
+
+DROP TRIGGER IF EXISTS IdadeClientes;
+
+CREATE TRIGGER IdadeClientes
+AFTER INSERT ON Clientes
+BEGIN    
+    UPDATE Clientes
+    SET Idade = date('now') - (SELECT DataDeNascimento FROM Pessoas WHERE ID = NEW.IDpessoa)
+    WHERE IDpessoa = NEW.IDpessoa;
+END;
+
+-- 1.4 Preço dos bilhetes trigger
+
+DROP TRIGGER IF EXISTS PrecoBilhete;
+
+CREATE TRIGGER PrecoBilhete
+AFTER INSERT ON Bilhetes
+BEGIN    
+    UPDATE Bilhetes
+    SET Preco = (julianday(DataFim) - julianday(DataInicio))*(SELECT PrecoDiario FROM TipoDeBilhete WHERE TypeName = NEW.TypeName)
+    WHERE Nbilhete = NEW.Nbilhete;
 END;
--- gatilho1_remove.sql
+++ gatilho1_remove.sql
@@ -1,1 +1,10 @@
+--1.1
 DROP TRIGGER RatingMedio;
+--1.2
+DROP TRIGGER SalarioPagamentos;
+--1.3
+DROP TRIGGER IdadeClientes;
+--1.4
+DROP TRIGGER PrecoBilhete;
+
+
